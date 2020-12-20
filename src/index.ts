9f3aaa28ee65bc194c5901d0ac304958d88e6e1f
Alvin Li <hins.xd@hotmail.com>
2020-12-20 15:27:13 +0800

    Remote stash #12318 -

--- src/index.ts
+++ src/index.ts
@@ -25,42 +25,46 @@ program.name("remote-stash");
 program
   .command("push [message]", { isDefault: true })
   .action(async (message: string = "") => {
-    const branchName = await exec("git branch --show-current").then((r) =>
-      r.stdout.trim()
-    );
+    try {
+      const branchName = await exec("git branch --show-current").then((r) =>
+        r.stdout.trim()
+      );
 
-    const match = branchName.match(stashBranchRegex);
-    const baseBranch = match ? match[1] : branchName;
-    if (!baseBranch) {
-      console.log(chalk.red("Branch name not found"));
-      process.exit(0);
+      const match = branchName.match(stashBranchRegex);
+      const baseBranch = match ? match[1] : branchName;
+      if (!baseBranch) {
+        console.log(chalk.red("Branch name not found"));
+        process.exit(0);
+      }
+
+      const stashesWithNumber = await exec(
+        `git for-each-ref --format='%(refname:short)' refs/heads/${baseBranch}-progress-[1-9]*`
+      ).then((r) => r.stdout.trim().split("\n"));
+
+      const maxStashBranch = stashesWithNumber.reduce(
+        (maxObj, branch) => {
+          const stashNum = getStashNo(branch);
+          if (!!stashNum && stashNum > maxObj.num)
+            return { name: branch, num: stashNum };
+          return maxObj;
+        },
+        { name: null, num: 0 } as { name: string | null; num: number }
+      );
+
+      const newStashNum = maxStashBranch.num + 1;
+      const newBranchName = `${baseBranch}-progress-${newStashNum}${
+        message ? `-${message}` : ""
+      }`;
+
+      await exec(`git checkout -b ${newBranchName}`);
+      await exec("git add -A");
+      await exec(
+        `git commit -m "Remote stash #${newStashNum} - ${message}" --no-verify`
+      );
+      await exec(`git checkout ${baseBranch}"`);
+    } catch (e) {
+      console.log(e);
     }
-
-    const stashesWithNumber = await exec(
-      `git for-each-ref --format='%(refname:short)' refs/heads/${baseBranch}-progress-[1-9]*`
-    ).then((r) => r.stdout.trim().split("\n"));
-
-    const maxStashBranch = stashesWithNumber.reduce(
-      (maxObj, branch) => {
-        const stashNum = getStashNo(branch);
-        if (!!stashNum && stashNum > maxObj.num)
-          return { name: branch, num: stashNum };
-        return maxObj;
-      },
-      { name: null, num: 0 } as { name: string | null; num: number }
-    );
-
-    const newStashNum = maxStashBranch.num + 1;
-    const newBranchName = `${baseBranch}-progress-${newStashNum}${
-      message ? `-${message}` : ""
-    }`;
-
-    await exec(`git checkout -b ${newBranchName}`);
-    await exec("git add -A");
-    await exec(
-      `git commit -m "Remote stash #${newStashNum} - ${message}" --no-verify`
-    );
-    await exec(`git checkout ${baseBranch}"`);
   });
 
 program.parse(process.argv);
